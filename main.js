ac36ef2ef944d8dd5845e1d38780cbfd8c3415ce
Pozdnya <kika_domination@yahoo.com>
2021-06-03 11:17:41 +0300

    hw2

--- main.js
+++ main.js
@@ -1,1 +1,52 @@
-console.log('test');
+//## Task #0
+const player1 = {
+  name: "Kitana",
+  hp: 120,
+  img: "http://reactmarathon-api.herokuapp.com/assets/kitana.gif",
+  weapon: "Steel fans",
+  attack: () => console.log(`${player1.name} Fight...`),
+};
+
+const player2 = {
+  name: "Sonya",
+  hp: 130,
+  img: "http://reactmarathon-api.herokuapp.com/assets/sonya.gif",
+  weapon: "Wind blade",
+  attack: () => console.log(`${player2.name} Fight...`),
+};
+
+function createPlayer(clas, {name, img, hp}) {
+  const $arenas = document.querySelector(".arenas");
+
+  const $player1 = document.createElement("div");
+  $player1.className = clas;
+  $arenas.appendChild($player1);
+
+  const $progressbar = document.createElement("div");
+  $progressbar.className = "progressbar";
+  $player1.appendChild($progressbar);
+
+  const $character = document.createElement("div");
+  $character.className = "character";
+  $player1.appendChild($character);
+
+  const $live = document.createElement("div");
+  $live.className = "live";
+  $live.style.width = "100%";
+  $live.textContent=hp
+  $progressbar.appendChild($live);
+
+  const $name = document.createElement("div");
+  $name.classList.add("name");
+  $name.textContent = name;
+  $progressbar.appendChild($name);
+
+  const $img = document.createElement("img");
+  $img.src = img;
+  $character.appendChild($img);
+}
+
+
+//## Task #2
+createPlayer('player1', player1);
+createPlayer('player2', player2);
